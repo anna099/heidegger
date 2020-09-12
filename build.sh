b763187a3aa104b0ec7d466d46e646a15c7f0604
Anna Hart <annahart099@gmail.com>
2020-09-12 19:00:04 +0100

    create template

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+chapters=('introduction')
+pdfs=( "${chapters[@]/%/.pdf}" )
+FN=x-footnotes.md
+TEMP=temp.html
+
+# HTML
+echo ${chapters[@]}
+for i in "${chapters[@]}"; do
+    if [ $i = ${chapters[0]} ]; then
+        pandoc $i/[1-99]*.md $i/$FN -o $TEMP
+	cp top-header.html index.html
+	cat contents.html >> index.html
+	cat $TEMP >> index.html
+	rm $TEMP
+    else
+        pandoc $i/[1-99]*.md $i/$FN -o $TEMP
+        cp header.html $i.html
+        cat contents.html >> $i.html
+        cat $TEMP >> $i.html
+        rm $TEMP
+    fi
+done
+
+# PDF
+for i in "${chapters[@]}"; do
+    pandoc --quiet -t html --css pdf.css -V papersize:a5 \
+        $i/[1-99]*.md $i/$FN -o pdf/$i.pdf
+done
+cd pdf
+pdfunite ${pdfs[@]} hegel.pdf
+cd ..
